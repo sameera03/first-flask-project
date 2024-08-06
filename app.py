6b4bc8dcd3d32df2b250f9e58f98bc0126ee2f48
sameera03 <71255204+sameera03@users.noreply.github.com>
2024-08-06 15:30:45 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
+
 
 @app.route('/')
 def hello_world():
-  return "Hello, Sameera!"
+  # return "Hello there"
+  return render_template("home.html")
+
 
 if __name__ == "__main__":
-  app.run(host = "0.0.0.0", debug=True)
+  app.run(host="0.0.0.0", debug=True)
